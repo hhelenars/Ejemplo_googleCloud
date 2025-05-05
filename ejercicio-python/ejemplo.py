67249a0b4f8031e515b850c23f3252f660110201
hhelenars <helenarodriguezsoria@gmail.com>
2025-05-05 19:23:02 +0200

    código modificado

--- ejercicio-python/ejemplo.py
+++ ejercicio-python/ejemplo.py
@@ -1,5 +1,12 @@
-def saludar(nombre):
-    print("Hola ", nombre)
+import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def saludar():
+    return "Hola Helena."
 
 if __name__ == "__main__":
-    saludar("Helena")
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
